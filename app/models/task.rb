3e2636f982ab642a25891ec04c09d85b63e290c3
Valeriya <lerasmailbox@yahoo.com>
2014-12-06 19:44:06 +0000

    attachment size validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,4 +5,5 @@ class Task < ActiveRecord::Base
    has_attached_file :document, :default_url => "/files/:style/missing.doc", :styles => { :thumb => ["32x32#", :png] }
    validates_attachment_content_type :document, :content_type => /\Aapplication/
    validates_attachment_file_name :document, :matches => [/pdf\Z/, /doc\Z/]
+  validates_attachment_size :document, :less_than=>1.megabyte
 end
