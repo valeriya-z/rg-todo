997db3e38fc9e60d8a1d8a4b64fa72128033bb3c
Valeriya <lerasmailbox@yahoo.com>
2015-02-05 18:46:46 +0000

    change fonts

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ActiveRecord::Base
    attr_accessible :name, :completed, :document, :document_file_name
    belongs_to :project
    validates :name, presence: true
-   has_attached_file :document, :default_url => "/files/:style/missing.doc", :styles => { :thumb => ["32x32#", :png] }
+   has_attached_file :document, :default_url => "/files/:style/missing.doc"
    validates_attachment_content_type :document, :content_type => /\Aapplication/
    validates_attachment_file_name :document, :matches => [/pdf\Z/, /doc\Z/]
   validates_attachment_size :document, :less_than=>1.megabyte
